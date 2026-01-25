59609b81413c0b3dc88ccd5538bfde39395b7e34
Andrea Crotti <andrea.crotti.0@gmail.com>
2026-01-25 21:47:49 +0000

    claude

---
+++ dot_config/private_fish/functions/claude.fish
@@ -0,0 +1,3 @@
+function claude --wraps='npx @anthropic-ai/claude-code --dangerously-skip-permissions' --description 'alias claude npx @anthropic-ai/claude-code --dangerously-skip-permissions'
+    npx @anthropic-ai/claude-code --dangerously-skip-permissions $argv
+end
