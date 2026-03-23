5fb2016c4a328b8e2f8b2649a492812a9b41eb20
Andrea Crotti <andrea.crotti.0@gmail.com>
2026-03-23 13:47:47 +0000

    add chezmoi configuration

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,15 +1,13 @@
-set PATH ~/scripts ~/.local/bin ~/.cargo/bin /var/lib/snapd/snap/bin ~/.config/emacs/bin ~/.screenlayout ~/scripts ~/.babashka/bbin/bin ~/go/bin ~/.fly/bin/ ~/.atuin/bin /usr/local/bin $PATH
+set PATH ~/scripts ~/.local/bin ~/.cargo/bin /var/lib/snapd/snap/bin ~/.config/emacs/bin ~/scripts ~/.babashka/bbin/bin ~/go/bin ~/.fly/bin/ ~/.atuin/bin /usr/local/bin $PATH
 
 if status is-interactive
     direnv hook fish | source
     atuin init fish | source
-    cowsay (fortune)
+    cowsay -f (cowsay -l | tail -n +2 | string split ' ' | shuf -n1) (fortune) | lolcat
     # Commands to run in interactive sessions can go here
     if type -q starship
         starship init fish | source
     end
 end
 
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f ~/.config/fish/google-cloud-sdk/path.fish.inc ]; . ~/.config/fish/google-cloud-sdk/path.fish.inc; end
 set LSP_USE_PLISTS true
