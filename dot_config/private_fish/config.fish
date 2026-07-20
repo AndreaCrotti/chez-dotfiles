ceb7850af09f0ad46e553a8d3fa9443fe5883456
Andrea Crotti <andrea.crotti.0@gmail.com>
2026-07-20 13:25:10 +0100

    cleanup and improvements

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -3,6 +3,10 @@ set PATH ~/scripts ~/.local/bin ~/.cargo/bin /var/lib/snapd/snap/bin ~/.config/e
 if status is-interactive
     direnv hook fish | source
     atuin init fish | source
+    fzf --fish | source
+    if type -q zoxide
+        zoxide init fish --cmd j | source
+    end
     cowsay -f (cowsay -l | tail -n +2 | string split ' ' | shuf -n1) (fortune) | lolcat
     # Commands to run in interactive sessions can go here
     if type -q starship
@@ -11,11 +15,13 @@ if status is-interactive
 end
 
 set LSP_USE_PLISTS true
+set -gx EDITOR nvim
+set -gx RIPGREP_CONFIG_PATH ~/.config/ripgrep/ripgreprc
 
 alias b 'bruno --enable-features=WaylandWindowDecorations --ozone-platform-hint=auto'
 alias e emacs
 alias g ghostty
-alias ls exa
+alias ls 'eza --icons'
 alias p python
 alias v nvim
 alias z zellij
